ecbe354f93d361754a65dad3a622eb4a0f7c89c2
JY Lee <jylee.rahman@gmail.com>
2025-10-01 11:42:03 -0400

    Merge conflict 2

--- app/routes.ts
+++ app/routes.ts
@@ -3,9 +3,7 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("signin", "routes/signin.tsx"),
-    route("protected", "routes/protected.tsx"),
     route("signup", "routes/signup.tsx"),
-    route("/api/auth/*", "routes/api.auth.$.ts"),
-    route("tests", "routes/test.tsx")
+    route("/api/auth/*", "routes/api.auth.$.ts")
 
 ] satisfies RouteConfig;
