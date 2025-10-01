8e0596d09755f9292af2f7c411b14051b0e1acc9
JY Lee <jylee.rahman@gmail.com>
2025-10-01 11:36:48 -0400

    Merge conflict

--- auth.server.ts
+++ auth.server.ts
@@ -3,15 +3,7 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "./src/db/db"
 import * as schema from './src/db/schema'
 
-<<<<<<< HEAD
-export const auth = betterAuth({
-    emailAndPassword: {
-        enabled: true,
-    },
-    database: drizzleAdapter(db, {
-        provider: "pg", // or "pg" or "mysql"
-=======
-debugger;
+
 export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
@@ -19,7 +11,6 @@ export const auth = betterAuth({
     },
     database: drizzleAdapter(db, {
         provider: "pg",
->>>>>>> client-server-react-router
         schema: schema,
     }),
 })
--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-<<<<<<< HEAD
-import App from './App.tsx'
-=======
 import App from './App'
 
 import { createBrowserRouter } from "react-router-dom";
 import Home from "app/routes/home";
-import { LogIn } from "~/routes/signin";
-import { SignUp } from "app/routes/signup";
-import { Protected } from "app/routes/protected";
+import LogIn from "app/routes/login"
+import SignUp from "app/routes/signup";
+import Protected from "app/routes/protected";
 
 export const router = createBrowserRouter([
   { path: "/", element: <Home /> },
@@ -19,7 +16,6 @@ export const router = createBrowserRouter([
   { path: "/protected", element: <Protected /> },
 ]);
 
->>>>>>> client-server-react-router
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
