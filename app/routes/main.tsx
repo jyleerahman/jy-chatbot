53d83e972dac1a2bd4d9851bdaaf620c7a742b11
JY Lee <jylee.rahman@gmail.com>
2025-10-04 11:02:44 -0400

    Edit more styling

--- app/routes/main.tsx
+++ app/routes/main.tsx
@@ -4,17 +4,24 @@ import { Link } from "react-router-dom"
 export default function Main() {
     return (
         <>
-            <div className="bg-[#68699B] w-full h-10 p-2 pl-3 text-[#FFFFFD]"> Home | About | Help | Corporate Services</div>
-            <div className="flex justify-center m-10 pl-10">
-                <Link to="/chat/1"><img width="250px" src="../app/font/andrew.png" />
-                    <p className="mt-4 ml-3 text-2xl underline text-blue-700">Ask Andrew</p>
-                </Link>
-                <Link to="/chat/2"><img width="250px" src="../app/font/paris.png" />
-                    <p className="mt-5 ml-3 text-2xl underline text-blue-700">Ask Paris</p>
-                </Link>
-                <Link to="/chat/3"><img width="250px" src="../app/font/david.png" />
-                    <p className="mt-5 ml-3 text-2xl underline text-blue-700">Ask David</p>
-                </Link>
+            <div className="flex flex-col">
+                <div className="flex items-center m-5">
+                    <img src="../app/font/fractal_logo.png" className="w-50" />
+                    <p className="ml-60 font-extrabold text-[#CB0302] text-[30px]">Are you <span className="text-[35px] text-shadow-sm">STUCK?</span><span className="text-[35px] text-shadow-sm"> ASK</span> your instructor!</p>
+                </div>
+
+
+                <div className="flex justify-center ml-30">
+                    <Link to="/chat/1"><img width="250px" src="../app/font/andrew.png" />
+                        <p className="mt-4 ml-3 text-2xl underline text-blue-700">Ask Andrew</p>
+                    </Link>
+                    <Link to="/chat/2"><img width="250px" src="../app/font/paris.png" />
+                        <p className="mt-5 ml-3 text-2xl underline text-blue-700">Ask Paris</p>
+                    </Link>
+                    <Link to="/chat/3"><img width="250px" src="../app/font/david.png" />
+                        <p className="mt-5 ml-3 text-2xl underline text-blue-700">Ask David</p>
+                    </Link>
+                </div>
             </div>
         </>
     )
